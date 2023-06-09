5349d4ea19a92da97adc3090cdcf388e1f152f83
Hueihuea <mchhui@foxmail.com>
2023-06-10 01:34:38 +0800

    引入EasyEffect

---
+++ src/main/java/mchhui/easyeffect/EasyEffect.java
@@ -0,0 +1,58 @@
+package mchhui.easyeffect;
+
+import java.util.function.Consumer;
+
+import io.netty.buffer.Unpooled;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.network.FMLEventChannel;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod(modid="easyeffect")
+public class EasyEffect {
+    public static FMLEventChannel channel;
+    
+    @EventHandler
+    public void onInit(FMLInitializationEvent event) {
+        channel=NetworkRegistry.INSTANCE.newEventDrivenChannel("easyeffect");
+        if(FMLCommonHandler.instance().getSide()==Side.CLIENT) {
+            EasyEffectClient client=new EasyEffectClient();
+            MinecraftForge.EVENT_BUS.register(client);
+            channel.register(client);
+        }
+    }
+    
+    @EventHandler
+    public void onServerStarting(FMLServerStartingEvent event) {
+        event.registerServerCommand(new EEComand());
+    }
+    
+    public static void sendEffect(EntityPlayerMP e, double x, double y, double z, double vx, double vy, double vz, double ax,
+            double ay, double az, int delay, int fps, int length, int unit, double size, String path) {
+        PacketBuffer buf = new PacketBuffer(Unpooled.buffer());
+        buf.writeDouble(x);
+        buf.writeDouble(y);
+        buf.writeDouble(z);
+        buf.writeDouble(vx);
+        buf.writeDouble(vy);
+        buf.writeDouble(vz);
+        buf.writeDouble(ax);
+        buf.writeDouble(ay);
+        buf.writeDouble(az);
+        buf.writeInt(delay);
+        buf.writeInt(fps);
+        buf.writeInt(length);
+        buf.writeInt(unit);
+        buf.writeDouble(size);
+        buf.writeString(path);
+        EasyEffect.channel.sendTo(new FMLProxyPacket(buf, "easyeffect"), e);
+    }
+}
