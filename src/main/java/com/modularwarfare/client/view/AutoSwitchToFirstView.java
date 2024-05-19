64aba38a039ce109104141e6ccb8912ff9e56ced
SIZStan <1014038476@qq.com>
2024-05-19 17:15:39 +0800

    修复不持枪也会切换视角的问题

--- src/main/java/com/modularwarfare/client/view/AutoSwitchToFirstView.java
+++ src/main/java/com/modularwarfare/client/view/AutoSwitchToFirstView.java
@@ -1,6 +1,8 @@
 package com.modularwarfare.client.view;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
@@ -15,42 +17,47 @@ import com.teamderpy.shouldersurfing.client.ShoulderInstance;
 
 public class AutoSwitchToFirstView {
 
+    public ItemStack heldItemstStack=ItemStack.EMPTY;
+
     private static boolean aimlock = false;
     private static boolean aimFlag = false;
     private static long lastAimTime;
 
     @SubscribeEvent
     public void onRenderTick(RenderTickEvent event) {
-        if (Minecraft.getMinecraft().player != null && ModConfig.INSTANCE.hud.autoSwitchToFirstView) {
-            boolean isMouseDown = Mouse.isButtonDown(1);
-
-            long time = System.currentTimeMillis();
-            if (isMouseDown) {
-                if (!aimFlag) {
-                    aimFlag = true;
-                    lastAimTime = time;
-                }
-            } else {
-                if (aimFlag) {
-                    if (time - lastAimTime < 200) {
-                        if (!aimlock && Minecraft.getMinecraft().gameSettings.thirdPersonView == 1) {
-                            aimlock = true;
-                        } else if (aimlock) {
-                            if (ClientProxy.shoulderSurfingLoaded) {
-                                Minecraft.getMinecraft().gameSettings.thirdPersonView = 1;
-                                 ShoulderInstance.getInstance().setShoulderSurfing(true);
-                            } else {
-                                Minecraft.getMinecraft().gameSettings.thirdPersonView = 1;
+        Item item = heldItemstStack.getItem();
+        if(item instanceof ItemGun) {
+            if (Minecraft.getMinecraft().player != null && ModConfig.INSTANCE.hud.autoSwitchToFirstView) {
+                boolean isMouseDown = Mouse.isButtonDown(1);
+    
+                long time = System.currentTimeMillis();
+                if (isMouseDown) {
+                    if (!aimFlag) {
+                        aimFlag = true;
+                        lastAimTime = time;
+                    }
+                } else {
+                    if (aimFlag) {
+                        if (time - lastAimTime < 200) {
+                            if (!aimlock && Minecraft.getMinecraft().gameSettings.thirdPersonView == 1) {
+                                aimlock = true;
+                            } else if (aimlock) {
+                                if (ClientProxy.shoulderSurfingLoaded) {
+                                    Minecraft.getMinecraft().gameSettings.thirdPersonView = 1;
+                                     ShoulderInstance.getInstance().setShoulderSurfing(true);
+                                } else {
+                                    Minecraft.getMinecraft().gameSettings.thirdPersonView = 1;
+                                }
+                                Minecraft.getMinecraft().renderGlobal.setDisplayListEntitiesDirty();
+                                aimlock = false;
                             }
-                            Minecraft.getMinecraft().renderGlobal.setDisplayListEntitiesDirty();
-                            aimlock = false;
                         }
                     }
+                    aimFlag = false;
                 }
-                aimFlag = false;
-            }
-            if (aimlock) {
-                Minecraft.getMinecraft().gameSettings.thirdPersonView = 0;
+                if (aimlock) {
+                    Minecraft.getMinecraft().gameSettings.thirdPersonView = 0;
+                }
             }
         }
     }
