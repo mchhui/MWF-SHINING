e3717c223e5c227ad15ae3cc06c8fc467a5cd0cf
KomiMoe <i@komi.moe>
2024-05-24 21:03:49 +0800

    修复MixinRenderPlayer爆WARN

--- src/main/java/com/modularwarfare/mixin/client/MixinRenderPlayer.java
+++ src/main/java/com/modularwarfare/mixin/client/MixinRenderPlayer.java
@@ -1,27 +1,24 @@
 package com.modularwarfare.mixin.client;
 
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.SoftOverride;
 
-import com.modularwarfare.ModConfig;
 import com.modularwarfare.core.MWFCoreHooks;
-import com.modularwarfare.utility.OptifineHelper;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.math.Vec3d;
-import net.optifine.shaders.MWFOptifineShadesHelper;
-import net.optifine.shaders.Shaders;
-import net.optifine.shaders.ShadersRender;
 
+@SideOnly(Side.CLIENT)
 @Mixin(RenderPlayer.class)
 public class MixinRenderPlayer {
+    /**
+     * @author KomiMoe
+     * @reason I don't know, but must a doc
+     */
     @Overwrite
-    protected void renderLivingAt(EntityLivingBase entityLivingBaseIn, double x, double y, double z) {
+    protected void renderLivingAt(AbstractClientPlayer entityLivingBaseIn, double x, double y, double z) {
         MWFCoreHooks.renderLivingAtForRenderPlayer(entityLivingBaseIn, x, y, z);
     }
 }
