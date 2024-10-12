3e77eb8da159dbfc6d5a0723aba351d99ade43b5
SIZStan <1014038476@qq.com>
2024-10-12 14:15:54 +0800

    调整fallback与动画优先机制

--- src/main/java/com/modularwarfare/client/fpp/enhanced/animation/EnhancedStateMachine.java
+++ src/main/java/com/modularwarfare/client/fpp/enhanced/animation/EnhancedStateMachine.java
@@ -286,6 +286,8 @@ public class EnhancedStateMachine {
             return !ItemGun.hasNextShot(heldItemstStack) && 
                    ((GunEnhancedRenderConfig)currentModel.config).animations.containsKey(AnimationType.FIRE_LAST_ADS)
                    ? AnimationType.FIRE_LAST_ADS
+                   : ((GunEnhancedRenderConfig)currentModel.config).animations.containsKey(AnimationType.FIRE_LAST)
+                   ? AnimationType.FIRE_LAST
                    : ((GunEnhancedRenderConfig)currentModel.config).animations.containsKey(AnimationType.FIRE_ADS)
                    ? AnimationType.FIRE_ADS
                    : AnimationType.FIRE;
@@ -302,6 +304,8 @@ public class EnhancedStateMachine {
             return !ItemGun.hasNextShot(heldItemstStack) && 
                    ((GunEnhancedRenderConfig)currentModel.config).animations.containsKey(AnimationType.POST_FIRE_ADS_EMPTY)
                    ? AnimationType.POST_FIRE_ADS_EMPTY
+                   : ((GunEnhancedRenderConfig)currentModel.config).animations.containsKey(AnimationType.POST_FIRE_EMPTY)
+                   ? AnimationType.POST_FIRE_EMPTY
                    : ((GunEnhancedRenderConfig)currentModel.config).animations.containsKey(AnimationType.POST_FIRE_ADS)
                    ? AnimationType.POST_FIRE_ADS
                    : AnimationType.POST_FIRE;
