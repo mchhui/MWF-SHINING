3001b440f96ae45e6ba41c2a07d68c1119b0a06c
SIZStan <1014038476@qq.com>
2025-02-10 15:46:56 +0800

    投掷发包位置移动

--- src/main/java/com/modularwarfare/common/network/PacketGrenadeThrow.java
+++ src/main/java/com/modularwarfare/common/network/PacketGrenadeThrow.java
@@ -1,12 +1,13 @@
-package com.modularwarfare.common.grenades;
+package com.modularwarfare.common.network;
 
 import com.modularwarfare.ModularWarfare;
 import com.modularwarfare.common.entity.grenades.EntityGrenade;
 import com.modularwarfare.common.entity.grenades.EntitySmokeGrenade;
 import com.modularwarfare.common.entity.grenades.EntityStunGrenade;
+import com.modularwarfare.common.grenades.GrenadeType;
+import com.modularwarfare.common.grenades.ItemGrenade;
 import com.modularwarfare.common.guns.WeaponAnimationType;
 import com.modularwarfare.common.init.ModSounds;
-import com.modularwarfare.common.network.PacketBase;
 import com.modularwarfare.common.guns.WeaponSoundType;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
@@ -62,67 +63,33 @@ public class PacketGrenadeThrow extends PacketBase {
         if (stack.getItem() instanceof ItemGrenade) {
             GrenadeType type = ((ItemGrenade) stack.getItem()).type;
             
-            float actualStrength = throwStrength * (isLowThrow ? type.throwStrengthLow : type.throwStrength);
-            
-            Vec3d throwVec;
-            if (!isLowThrow && type.animationType == WeaponAnimationType.ENHANCED) {
-                float pitch = player.rotationPitch - 15;
-                float yaw = player.rotationYaw;
-                float f = 0.017453292F;
-                
-                double x = -Math.sin(yaw * f) * Math.cos(pitch * f);
-                double y = -Math.sin(pitch * f);
-                double z = Math.cos(yaw * f) * Math.cos(pitch * f);
-                
-                throwVec = new Vec3d(x, y, z);
-            } else {
-                throwVec = player.getLookVec();
-            }
-            
-            double modifier = player.isSprinting() ? 1.25 : 1.0;
-            
-            double motionX = ((throwVec.x * 1.5) * modifier) * actualStrength;
-            double motionY = ((throwVec.y * 1.5) * modifier) * actualStrength;
-            double motionZ = ((throwVec.z * 1.5) * modifier) * actualStrength;
+            float strength = throwStrength * (isLowThrow ? type.throwStrengthLow : type.throwStrength);
             
             switch (type.grenadeType) {
                 case Frag:
-                    EntityGrenade grenade = new EntityGrenade(player.world, player, actualStrength, type);
+                    EntityGrenade grenade = new EntityGrenade(player.world, player, strength, type, isLowThrow);
                     if(remainingFuseTime > 0) {
                         grenade.fuse = (int)(remainingFuseTime * 20);
                     }
-                    grenade.motionX = motionX;
-                    grenade.motionY = motionY;
-                    grenade.motionZ = motionZ;
-                    player.world.playSound(null, player.posX, player.posY, player.posZ, ModSounds.GRENADE_THROW, SoundCategory.PLAYERS, 1.0f, 1.0f);
+                    type.playSound(player, WeaponSoundType.GrenadeThrow, stack);
                     player.world.spawnEntity(grenade);
                     break;
                 case Smoke:
-                    EntitySmokeGrenade smoke = new EntitySmokeGrenade(player.world, player, actualStrength, type);
+                    EntitySmokeGrenade smoke = new EntitySmokeGrenade(player.world, player, strength, type, isLowThrow);
                     if(remainingFuseTime > 0) {
                         smoke.fuse = (int)(remainingFuseTime * 20);
                     }
-                    smoke.motionX = motionX;
-                    smoke.motionY = motionY;
-                    smoke.motionZ = motionZ;
-                    player.world.playSound(null, player.posX, player.posY, player.posZ, ModSounds.GRENADE_THROW, SoundCategory.PLAYERS, 1.0f, 1.0f);
+                    type.playSound(player, WeaponSoundType.GrenadeThrow, stack);
                     player.world.spawnEntity(smoke);
                     break;
                 case Stun:
-                    EntityStunGrenade stun = new EntityStunGrenade(player.world, player, actualStrength, type);
+                    EntityStunGrenade stun = new EntityStunGrenade(player.world, player, strength, type, isLowThrow);
                     if(remainingFuseTime > 0) {
                         stun.fuse = (int)(remainingFuseTime * 20);
                     }
-                    stun.motionX = motionX;
-                    stun.motionY = motionY;
-                    stun.motionZ = motionZ;
-                    player.world.playSound(null, player.posX, player.posY, player.posZ, ModSounds.GRENADE_THROW, SoundCategory.PLAYERS, 1.0f, 1.0f);
+                    type.playSound(player, WeaponSoundType.GrenadeThrow, stack);
                     player.world.spawnEntity(stun);
                     break;
-            }
-            
-            if (!player.capabilities.isCreativeMode) {
-                stack.shrink(1);
             }
         }
     }
