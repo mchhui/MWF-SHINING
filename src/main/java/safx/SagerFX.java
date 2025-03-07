0938062ebfbefda4cebb118e6a809bc507280445
SIZStan <1014038476@qq.com>
2025-03-08 00:07:03 +0800

    移除特效forge强制要求

--- src/main/java/safx/SagerFX.java
+++ src/main/java/safx/SagerFX.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import safx.init.ISAInitializer;
 
-@Mod(modid = SagerFX.MODID, version = SagerFX.VERSION, name=SagerFX.NAME, acceptedMinecraftVersions=SagerFX.MCVERSION, /*guiFactory=SagerFX.GUI_FACTORY, updateJSON=SagerFX.UPDATEURL,*/ dependencies=SagerFX.DEPENDENCIES)
+@Mod(modid = SagerFX.MODID, version = SagerFX.VERSION, name=SagerFX.NAME, acceptedMinecraftVersions=SagerFX.MCVERSION/*, guiFactory=SagerFX.GUI_FACTORY, updateJSON=SagerFX.UPDATEURL, dependencies=SagerFX.DEPENDENCIES*/)
 public class SagerFX
 {
     public static final String MODID = "safx";
@@ -23,7 +23,7 @@ public class SagerFX
     public static final String VERSION = "1.0.0.0";
     public static final String NAME = "SagerFX";
     public static final String FORGE_BUILD = "14.23.5.2860";
-    public static final String DEPENDENCIES = "required:forge@["+FORGE_BUILD+",)";
+    //public static final String DEPENDENCIES = "required:forge@["+FORGE_BUILD+",)";
     
     @Mod.Instance
     public static SagerFX instance;
