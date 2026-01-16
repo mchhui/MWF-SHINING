ad82bdc7de6a6d94f52001098c2e130e0fed9e0d
SIZStan <1014038476@qq.com>
2026-01-16 12:17:09 +0800

    fix build

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 set /p version=Input version:
 call build.bat
 if not defined version (
-java  -jar ./MWFPackager.jar ./build/libs/modularwarfare-shining-snapshot.jar mchhui/modularmovements/
+java  -jar ./MWFPackager.jar ./build/libs/modularwarfare-shining-snapshot.jar mchhui/hebridge/
 )else (
-java  -jar ./MWFPackager.jar ./build/libs/modularwarfare-shining-snapshot.jar mchhui/modularmovements/ %version%
+java  -jar ./MWFPackager.jar ./build/libs/modularwarfare-shining-snapshot.jar mchhui/hebridge/ %version%
 )
